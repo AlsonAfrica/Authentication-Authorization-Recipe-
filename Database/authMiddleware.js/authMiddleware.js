75edbd7299e9177920653f449787d740e758ca56
Nhlakanipho <nhalakaniphoradebe337@gmail.com>
2024-10-30 09:56:06 +0200

    Refractoring: Validate recipe fields on recipe.js file

--- Database/authMiddleware.js/authMiddleware.js
+++ Database/authMiddleware.js/authMiddleware.js
@@ -3,25 +3,22 @@ import User from "../models/user.js";
 
 const protect = async (req, res, next) => {
     let token;
-    console.log(req.headers.authorization); // Log the token for debugging
+    console.log("This is the user Token",req.headers.authorization); // Log the token for debugging
+
     // Check if the Authorization header exists and starts with "Bearer"
     if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
         try {
             // Extract the token from the "Bearer <token>" format
             token = req.headers.authorization.split(" ")[1];
-            
-          
 
             // Verify the token using jwt.verify
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-            req.user = await User.findById(decoded.id).select("password")
-            next()
+            // Find the user by ID and exclude the password field
+            req.user = await User.findById(decoded.id).select("-password");
 
-            // Optional: Attach the decoded token payload (e.g., user info) to the req object
-            // req.user = decoded; 
-
-             // Proceed to the next middleware
+            // Proceed to the next middleware
+            next();
         } catch (error) {
             console.error("Token verification failed:", error);
             res.status(401).json({ error: "Not authorized, token failed" });
--- Database/models/recipe.js
+++ Database/models/recipe.js
@@ -1,35 +1,40 @@
 import mongoose from "mongoose";
 
-// Define schema
+// Define schema with field validation
 const recipesSchema = new mongoose.Schema(
     {
         name: {
             type: String,
-            required: true
+            required: [true, "Recipe name is required"],
+            minlength:[3,"Name must be at least 3 characters long"],
+            maxLength:[100,"Name must be at least 100 characters long"]
         },
         ingredients: [
             {
                 name: {
                     type: String,
-                    required: true
+                    required: [true,"Ingredients name is required"],
+                    minlength: [2,"Ingredient name must be at least 2 characters long"]
                 },
                 quantity: {
                     type: String, 
-                    required: false
+                    required: false,
+                    maxlength:[20, "Quantity must be less than 20 characters"]
                 }
             }
         ],
         instructions: {
             type: String,
-            required: true
+            required: [true, "Instructions are required"],
+            minlength: [10, "Instructions must be at least 10 characters long"]
         },
         preparationTime: {
             type: String,
-            required: true
+            required: [true, "Preparation time is required"],
         },
         servings: {
             type: String,
-            required: true
+            required: [true, "Servings are required"],
         },
         created_at: {
             type: Date,
